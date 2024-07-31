ef1485759f92a1706a242e04a949ce120d20af4b
JaysDevkule <jayshree.2298@gmail.com>
2024-07-31 21:07:37 +0530

    created assignment 02
    variable declaration, initialization, and typeOf operator

---
+++ Assignment02.js
@@ -0,0 +1,31 @@
+console.log("=============1===========");
+var bankName;
+console.log("Before initialization : ",bankName);
+bankName ="SBI Bank";
+console.log("After initialization : ",bankName);
+var bankNameType =typeof bankName;
+console.log("type of bankName is : ",bankNameType);
+
+console.log("=============2===========");
+ var marks="90%";
+ console.log("mark is : ",marks);
+ var mark1= typeof marks;
+ console.log(" typeof mark is : ",mark1);
+
+ console.log("=============3===========");
+
+ var isWorking="True";
+ console.log("is it working : ",isWorking);
+ var TypeIiWorking =typeof isWorking;
+ console.log("typeof isWorking : ",TypeIiWorking);
+
+ console.log("=============4===========");
+ var totalCount ="Hundred and seven";
+ console.log("=============a===========");
+ console.log("The value is : ",totalCount ,"  ",  "datatype is : ",typeof totalCount);
+
+ console.log("=============b===========");
+ var count="107";
+ console.log("The updated value is : ",count ,"  ",  "datatype is : ",typeof count);
+
+
