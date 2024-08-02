8bca296ff373a3b6a872da1252b22159da52cf95
JaysDevkule <jayshree.2298@gmail.com>
2024-08-02 21:15:16 +0530

    create assignment 3
    function declaration and function invocatation

---
+++ Assignment03.js
@@ -0,0 +1,24 @@
+console.log("=======1.======");
+function MyFunction1() {
+    console.log("My First Function execuated");
+}
+function MyFunction2() {
+    console.log("My Second Function execuated");
+}
+MyFunction1();
+MyFunction2();
+console.log("=======2.======");
+function personalDetails(firstName,lastName,clgName) {
+    console.log(" My First Name is : ",firstName);
+    console.log("My Name is : ",lastName);
+    console.log("My College Name is : ",clgName);
+}
+personalDetails("Jayshree","Kamble","SVERI");
+console.log("=======3.======");
+function addThreeValues(a,b,c) {
+    var result=a+b+c;
+    console.log("Addition is :",result);
+    
+} 
+addThreeValues(10.23,600,40);
+addThreeValues("Hello","Good","Morning");
