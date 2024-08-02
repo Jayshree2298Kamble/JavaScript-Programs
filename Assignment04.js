519dc51215b74b02c22d72ab72644174c5559156
JaysDevkule <jayshree.2298@gmail.com>
2024-08-02 22:35:12 +0530

    create assignment 4
    function declaration and initialization, and their type of function

---
+++ Assignment04.js
@@ -0,0 +1,13 @@
+console.log("=======1.======");
+function BankDetails(a,b,c,d) {
+    console.log("The Bank name is : ",a);
+    console.log("The Account name is : ",b);
+    console.log("The Location is : ",c);
+    console.log("The Pincode is : ",d);
+}
+console.log("****************Branch 1**************");
+BankDetails("CITI","433698463829",'Pune',411014);
+console.log("****************Branch 2**************");
+BankDetails("AXIS Bank","763698463829",'Mumbai',581014);
+console.log("****************Branch 3**************");
+BankDetails("HDFC Bank","467798463829",'Pune',411614);
