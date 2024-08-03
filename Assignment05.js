d24716169432c1dc204e0402c1ab2f2d08ca2c78
JaysDevkule <jayshree.2298@gmail.com>
2024-08-03 13:08:19 +0530

    create assignment 5
    function, function with no argument and no return value ,string ,string methods

---
+++ Assignment05.js
@@ -0,0 +1,17 @@
+console.log("=======2.======");
+function stringBasics() {
+    var str1="My dream company name is "
+    var str2="Google";
+    console.log(str1.concat(str2));
+}
+stringBasics();
+console.log("=======2.======");
+function MyHobbies(hobby1,hobby2,hobby3) {
+    console.log("my first hobby is ",hobby1);
+    console.log("my second hobby is ",hobby2);
+    console.log("my third hobby is ",hobby3);
+    console.log("sum of",hobby1," :",hobby1.length);
+    console.log("sum of",hobby2," :",hobby2.length);
+    console.log("sum of",hobby3," :",hobby3.length);
+}
+MyHobbies("Reading","Swimming","Photography");
