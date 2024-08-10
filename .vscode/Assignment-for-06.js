f21211156a25a6aaa0b87b2f92004e0d9fe20dd7
JaysDevkule <jayshree.2298@gmail.com>
2024-08-10 16:58:12 +0530

    created assignment 06
    control flow statement - if-else, for loop

---
+++ .vscode/Assignment-for-06.js
@@ -0,0 +1,41 @@
+// console.log(`***********1**********`);
+// // for (let i = 5; i <=15 ; i++) {
+    
+// //     console.log(i);
+    
+// // }
+// console.log(`***********2**********`);
+// for (let i =50; i >=40; i--) {
+    
+//          console.log(i);
+// }
+// console.log(`***********3**********`);
+// for (var x=0; x<=15; x++) {
+//     if (x % 2 != 0) {
+//             console.log(x);   
+//     }
+// }
+// console.log(`***********4**********`);
+// for (var x=0; x<=20; x++) {
+//     if (x % 2 == 0) {
+//             console.log(x);   
+//     }
+// }
+// console.log(`***********5**********`);
+// for (var x=5; x<=50; x=x+5) {
+//     {
+//             console.log(x);   
+//     }
+// }
+// console.log(`***********6**********`);
+// for (var x=10; x<=100; x=x+10) {
+//     {
+//             console.log(x);   
+//     }
+// }
+console.log(`***********6**********`);
+for (var x=100; x>=10; x=x-10) {
+    {
+            console.log(x);   
+    }
+}
