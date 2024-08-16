5da466f7434cc9ace18b97666b9d976249648cc2
JaysDevkule <jayshree.2298@gmail.com>
2024-08-17 02:22:02 +0530

    create assignment B
    object

---
+++ .vscode/Assignment-object-B.js
@@ -0,0 +1,47 @@
+console.log(`--------------------sbi bank details----------------------`);
+
+const sbiBank={
+    bankName:"sbiBank",
+    location:"Pune",
+    accNumber:"1111",
+    ifsc:"A",
+    interestRate:"8"
+}
+function showDetails1() {console.log(sbiBank)}
+showDetails1();
+
+console.log(`---------------------------axis bank details----------------------`);
+
+const axisBank={
+    bankName:"axisBank",
+    location:"solapur",
+    accNumber:"222",
+    ifsc:"B",
+    interestRate:"8.5"
+}
+function showDetails2() {console.log(axisBank)}
+
+showDetails2();
+console.log(`------------------------------hdfc bank details------------------------`);
+
+const hdfcBank={
+    bankName:"hdfcBank",
+    location:"Thane",
+    accNumber:"333",
+    ifsc:"C",
+    interestRate:"7"
+}
+function showDetails3() {console.log(hdfcBank)}
+showDetails3();
+
+console.log(`----------------------------yes bank details---------------------------------`);
+
+const yesBank={
+    bankName:"yesBank",
+    location:"Nagpur",
+    accNumber:"444",
+    ifsc:"D",
+    interestRate:"7.5"
+}
+function showDetails4() {console.log(yesBank)}
+showDetails4();
